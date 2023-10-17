819260c64a95df7ac5fe72888700fae7f7255e11
Patrick Gavigan <Patrick.Gavigan@hc-sc.gc.ca>
2023-10-17 13:01:45 -0400

    remove test case files that are not used

--- tests/test_Table_Detector.py
+++ tests/test_Table_Detector.py
@@ -8,7 +8,7 @@ def run_Table_Detector_to_excel():
     from table_processing.Table_Detector import Table_Detector
     if os.path.exists("all_excel.xlsx"):
         os.remove("all_excel.xlsx")
-    table_detector = Table_Detector(filename = "tests/resources/test_files/multipletab.pdf")
+    table_detector = Table_Detector(filename = "tests/resources/multipletab.pdf")
     table_detector.to_excel()
     yield os.path.exists("all_excel.xlsx")
     os.remove("all_excel.xlsx")

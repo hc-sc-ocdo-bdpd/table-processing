ac81cdce5628d5106fd405c5c70143e300ba9420
Daisy Xiao <daisysxiao@gmail.com>
2023-10-31 15:20:53 -0400

    Created excel_tables_comparison.py and function
    Also added use_tools folder for users to be able to put their files in for input for when they use this tool.

---
+++ table_processing/excel_tables_comparison.py
@@ -0,0 +1,100 @@
+from table_metrics import test_tables
+import pandas as pd
+import openpyxl
+from openpyxl.utils.dataframe import dataframe_to_rows
+import numpy as np
+
+def compare_tables(file1, file2):
+    '''
+    Compares two Excel files and generates a new Excel file with comparison 
+    results of the tables within each sheet. Input should be the file name,
+    and Excel files should be stored in use_tools > excel_table_comparison > excel_files.
+    Results are generated in use_tools > excel_table_comparison.
+    '''
+    
+    # Import Excel file paths
+    file_path_original = './use_tools/excel_table_comparison/excel_files/' + file1 + '.xlsx'
+    file_path_comparison = './use_tools/excel_table_comparison/excel_files/' + file2 + '.xlsx'
+    original_wb = openpyxl.load_workbook(file_path_original)
+    comparison_wb = openpyxl.load_workbook(file_path_comparison)
+
+    # Create a new workbook and worksheet
+    workbook = openpyxl.Workbook()
+    sheet = workbook.active
+    sheet.title = "Summary"
+    sheet.append(['Sheet name', '% Correct'])  # Add column titles to the "Main" sheet
+
+    # Go through each sheet in the original workbook
+    for sheet_name in original_wb.sheetnames:
+        if sheet_name in comparison_wb.sheetnames:
+            original_df = pd.read_excel(file_path_original, sheet_name=sheet_name)
+            comparison_df = pd.read_excel(file_path_comparison, sheet_name=sheet_name)
+
+            # Create a dataframe that contains True where the two tables are equal and False where they are not
+            differences_df = pd.DataFrame(comparison_df.values == original_df.values, index=original_df.index, columns=original_df.columns)
+
+            # Write the sheet name to the worksheet
+            new_sheet = workbook.create_sheet(title=sheet_name)
+
+            # Write the second table to the worksheet
+            rows = dataframe_to_rows(comparison_df, index=False, header=True)
+            for row in rows:
+                new_sheet.append(row)
+
+            # Format the cells that are different from the original table
+            for row in new_sheet.iter_rows(min_row=new_sheet.max_row-len(comparison_df)+1, min_col=1):
+                for cell in row:
+                    # Check if the cell is different from the original table
+                    list_of_rows = differences_df.values
+                    if list_of_rows[cell.row-new_sheet.max_row+len(comparison_df)-1, cell.column-1] == False:
+                        # If the cell is different, highlight it with yellow
+                        cell.fill = openpyxl.styles.PatternFill(start_color="FFFF00", end_color="FFFF00", fill_type="solid")
+
+            # Calculate the statistics
+            num_cells = original_df.size
+            num_true = differences_df.sum().sum()
+            overall_percent_diff = 100 - (num_true / num_cells * 100)
+            percent_true = differences_df.mean().mean() * 100
+            median_true = np.median(differences_df) * 100
+            min_true = np.min(differences_df) * 100
+            max_true = np.max(differences_df) * 100
+            range_true = max_true - min_true
+
+            # Write the statistics to the worksheet
+            new_sheet.append([])
+            new_sheet.append(['Number of Correct Cells', num_true])
+            new_sheet.append(['% Correct', percent_true])
+            new_sheet.append(['Number of Incorrect Cells', num_cells - num_true])
+            new_sheet.append(['% Different (Incorrect)', overall_percent_diff])
+            new_sheet.append([])
+            new_sheet.append(['Summary Statistics:'])
+            new_sheet.append(['Median % Correct', median_true])
+            new_sheet.append(['Min % Correct', min_true])
+            new_sheet.append(['Max % Correct', max_true])
+            new_sheet.append(['Range Correct', range_true])
+            new_sheet.append([])
+            new_sheet.append(['Comparison metrics:'])
+
+            # Calculate comparison metrics using the test_tables function
+            metrics_df = test_tables({sheet_name: [original_df,comparison_df]})
+
+            # Write comparison metrics to the Excel file
+            rows = dataframe_to_rows(metrics_df, index=False, header=True)
+            for row in rows:
+                new_sheet.append(row)
+
+            # Write sheet name and % Correct to "Main" sheet
+            sheet.append([sheet_name, percent_true])
+        else:
+            print(f"{sheet_name} not found in {file2}")
+
+    # Save the workbook to a new file
+    save_path = './use_tools/excel_table_comparison/' + file1 + '_and_' + file2 + '_comparison_results.xlsx'
+    workbook.save(save_path)
+
+# Get file names from user input
+first_file = input("Enter the name of the first Excel file: ")
+second_file = input("Enter the name of the second Excel file: ")
+
+# Use the function to compare the files
+compare_tables(first_file, second_file)
